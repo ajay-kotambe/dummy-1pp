f6637a28170d3a408e574f609eb102fc31d07eed
ajay-kotambe <kotambeajay07@gmail.com>
2025-08-04 17:34:40 +0530

    used useEffect added Homework component...

--- src/App.js
+++ src/App.js
@@ -1,8 +1,42 @@
-import logo from "./logo.svg";
+import { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
-  return <div className=""></div>;
+  const [text, setText] = useState("");
+  // ? first variation of useEffect -> every render
+  // useEffect(()=>{
+  //   console.log("Console Rendering done...")
+  // })
+
+  //? second variation -> first render
+  // useEffect(() => {
+  //   console.log("Console Rendering done...");
+  // },   []);
+
+  //? third  variation -> first render + whenever dependency changes...
+  // useEffect(() => {
+  //   console.log("text changed observed...");
+  // }, [text]);
+
+  //? fourth  variation -> to handle unmounting of component...
+  useEffect(() => {
+    console.log("listener added.."); //! this runs second...
+    return () => {
+      console.log("listener removed...."); //! this runs first
+    };
+  }, [text]);
+ 
+  const changeHandler = (event) => {
+    // event.preventDefault();
+    setText(event.target.value);
+    console.log(text);
+  };
+
+  return (
+    <div className="App">
+      <input type="text" onChange={changeHandler} />
+    </div>
+  );
 }
 
 export default App;
